1de6b14f7dd2c78ca43fa9f7917c646ac3730534
Tittu Varghese <tittu.parcx@gmail.com>
2018-03-02 15:10:57 +0530

    Minor fixes

--- blockchain/setup.go
+++ blockchain/setup.go
@@ -4,20 +4,30 @@ import (
 	"fmt"
 	"time"
 
+	"github.com/hyperledger/fabric-sdk-go/api/apitxn/chclient"
 	chmgmt "github.com/hyperledger/fabric-sdk-go/api/apitxn/chmgmtclient"
+	resmgmt "github.com/hyperledger/fabric-sdk-go/api/apitxn/resmgmtclient"
 	"github.com/hyperledger/fabric-sdk-go/pkg/config"
+	packager "github.com/hyperledger/fabric-sdk-go/pkg/fabric-client/ccpackager/gopackager"
 	"github.com/hyperledger/fabric-sdk-go/pkg/fabsdk"
+	"github.com/hyperledger/fabric-sdk-go/third_party/github.com/hyperledger/fabric/common/cauthdsl"
 )
 
 // FabricSetup implementation
 type FabricSetup struct {
-	ConfigFile    string
-	OrgID         string
-	ChannelID     string
-	initialized   bool
-	ChannelConfig string
-	OrgAdmin      string
-	OrgName       string
+	ConfigFile      string
+	OrgID           string
+	ChannelID       string
+	ChainCodeID     string
+	initialized     bool
+	ChannelConfig   string
+	ChaincodeGoPath string
+	ChaincodePath   string
+	OrgAdmin        string
+	OrgName         string
+	client          chclient.ChannelClient
+	admin           resmgmt.ResourceMgmtClient
+	sdk             *fabsdk.FabricSDK
 }
 
 // Initialize reads the configuration file and sets up the client, chain and event hub
@@ -51,7 +61,7 @@ func (setup *FabricSetup) Initialize() error {
 	orgAdminUser := session
 
 	// Create channel
-	req := chmgmt.SaveChannelRequest{ChannelID: setup.ChannelID, ChannelConfig: setup.ChannelConfig + "chainhero.channel.tx", SigningIdentity: orgAdminUser}
+	req := chmgmt.SaveChannelRequest{ChannelID: setup.ChannelID, ChannelConfig: setup.ChannelConfig + "channel.tx", SigningIdentity: orgAdminUser}
 	if err = chMgmtClient.SaveChannel(req); err != nil {
 		return fmt.Errorf("failed to create channel: %v", err)
 	}
@@ -71,5 +81,42 @@ func (setup *FabricSetup) Initialize() error {
 	}
 
 	fmt.Println("Initialization Successful")
+	return nil
+}
+
+func (setup *FabricSetup) InstallAndInstantiateCC() error {
+
+	// Create chaincode package for our chaincode
+	ccPkg, err := packager.NewCCPackage(setup.ChaincodePath, setup.ChaincodeGoPath)
+	if err != nil {
+		return fmt.Errorf("failed to create chaincode package: %v", err)
+	}
+
+	// Install our chaincode on org peers
+	installCCReq := resmgmt.InstallCCRequest{Name: setup.ChainCodeID, Path: setup.ChaincodePath, Version: "1.0", Package: ccPkg}
+	_, err = setup.admin.InstallCC(installCCReq)
+	if err != nil {
+		return fmt.Errorf("failed to install cc to org peers %v", err)
+	}
+
+	// Set up chaincode policy
+	ccPolicy := cauthdsl.SignedByAnyMember([]string{"org1.servntire.com"})
+
+	// Org resource manager will instantiate our chaincode on the channel
+	err = setup.admin.InstantiateCC(setup.ChannelID, resmgmt.InstantiateCCRequest{Name: setup.ChainCodeID, Path: setup.ChaincodePath, Version: "1.0", Args: [][]byte{[]byte("init")}, Policy: ccPolicy})
+	if err != nil {
+		return fmt.Errorf("failed to instantiate the chaincode: %v", err)
+	}
+
+	// Channel client is used to query and execute transactions
+	setup.client, err = setup.sdk.NewClient(fabsdk.WithUser("User1")).Channel(setup.ChannelID)
+	if err != nil {
+		return fmt.Errorf("failed to create new channel client: %v", err)
+	}
+
+	// Clean the client
+	defer setup.client.Close()
+
+	fmt.Println("Chaincode Installation & Instantiation Successful")
 	return nil
 }
